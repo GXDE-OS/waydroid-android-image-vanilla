396eb042253cc8930efdafef171a240550f36d59
gfdgd_xi <3025613752@qq.com>
2023-08-05 17:24:17 +0800

    c

--- build-python3-gbinder.sh
+++ build-python3-gbinder.sh
@@ -2,9 +2,9 @@
 version=$1
 arch=$2
 systemPath=debian-$version
-chrootRun=sudo chroot $systemPath
+chrootRun="sudo chroot $systemPath"
 sudo apt install debootstrap -y
-sudo debootstrap $version --arch=$2 $systemPath
+sudo debootstrap $version --arch=$arch $systemPath
 sudo aria2c -d /usr/bin -o pardus-chroot https://github.com/gfdgd-xi/deep-wine-runner/raw/main/pardus-chroot
 sudo chmod 777 /usr/bin/pardus-chroot
 sudo pardus-chroot $systemPath
