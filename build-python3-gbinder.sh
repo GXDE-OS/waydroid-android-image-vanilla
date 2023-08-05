f532e7a68e70510a4cd6c05a220729b6f24165a0
gfdgd_xi <3025613752@qq.com>
2023-08-05 17:40:35 +0800

    lib

--- build-python3-gbinder.sh
+++ build-python3-gbinder.sh
@@ -15,7 +15,7 @@ sudo mkdir $systemPath/deb
 aria2c https://github.com/gfdgd-xi/waydroid-deb/raw/main/focal/libgbinder-dev_1.1.25_amd64.deb -d $systemPath/deb
 aria2c https://github.com/gfdgd-xi/waydroid-deb/raw/main/focal/libgbinder_1.1.25_amd64.deb -d $systemPath/deb
 aria2c https://github.com/gfdgd-xi/waydroid-deb/raw/main/focal/libglibutil-dev_1.0.66_amd64.deb -d $systemPath/deb
-aria2c https://github.com/gfdgd-xi/waydroid-deb/blob/main/focal/libglibutil_1.0.66_amd64.deb -d $systemPath/deb
+aria2c https://github.com/gfdgd-xi/waydroid-deb/raw/main/focal/libglibutil_1.0.66_amd64.deb -d $systemPath/deb
 $chrootRun bash -c 'apt install /deb/*.deb'
 $chrootRun bash -c 'cd gbinder-python ; apt build-dep .'
 $chrootRun bash -c 'cd gbinder-python ; dpkg-buildpackage -b'
